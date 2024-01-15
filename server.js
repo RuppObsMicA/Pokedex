05c36c57c5c2722012aafca00b25342ef63b7099
RomanShalkouski <ShalkovskyRG@gmail.com>
2024-01-15 20:37:10 +0300

    Fixed async access to DB

--- server.js
+++ server.js
@@ -1,60 +1,67 @@
 const http = require('http');
-const syncMysql = require('sync-mysql');
-
-const syncConnection = new syncMysql({
-    host: "localhost",
-    user: "root",
-    database: "test",
-    password: "root"
-});
-
 const PORT = 3000;
+const mysql = require('mysql');
 
-let query = "SELECT * FROM pokemon";
+const query = "SELECT * FROM pokemon";
 
 const server = http.createServer((req, res) =>{
     console.log('Server request');
     console.log(req.url, req.method);
 
-    res.writeHead(201, {'Content-Type': 'application/json'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-    const result = syncConnection.query(query);
 
-    res.write(JSON.stringify(result));
-    res.end();
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "test",
+        password: "root"
+    });
 
+    connection.connect( err => {
+        if (err) {
+            console.log(err);
+            return err
+        } else {
+            console.log("Database works");
+        }
+    });
+
+    (async function (){
+        let promise = new Promise((resolve, reject) => {
+            connection.query(query, function (err, results) {
+                if (err) console.log(err);
+                resolve(results);
+            });
+        });
+        let result = await promise;
+        sendResponse(result);
+        connection.end();
+    })();
+
+    // Second version of async code
+
+    // let promise = new Promise((resolve, reject) => {
+    //     connection.query(query, function (err, results) {
+    //         if (err) console.log(err);
+    //         resolve(results);
+    //     });
+    // })
+    // promise.then(result => {
+    //     sendResponse(result);
+    //     connection.end();
+    // });
+
+    function sendResponse (response){
+        res.write(JSON.stringify(response));
+        res.end();
+    }
 });
 
 server.listen(PORT, 'localhost', (error) =>{
     error ? console.log(error): console.log("Listening port 3000");
 });
 
-// Database
-// const mysql = require('mysql');
-//
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     database: "test",
-//     password: "root"
-// })
-//
-// connection.connect( err => {
-//     if (err) {
-//         console.log(err);
-//         return err
-//     } else {
-//         console.log("Database works");
-//     }
-// })
-//
-// connection.query(query, function(err, results) {
-//     if(err) console.log(err);
-//
-//     toSend = results[0];
-//
-//     connection.end();
-// });
 
 
 
